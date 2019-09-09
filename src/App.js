17d33eb6b7875c9009a4af13bf453f570fe53e90
Nikita Parkhomenko <nikita.parkhom@gmail.com>
2019-09-09 15:53:15 +0300

    Create Components directory

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React, {Component} from 'react';
 import './App.css';
 
+import Header from './Components/Header/Header'
+
 class App extends Component {
+
+  state = {
+
+  }
 
   render() {
     return(
       <div className="App">
+        <Header />
       </div>
     )
   }
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = (props) => {
+    return(
+        <div className="Header">
+            <h1>Hello from your Task Manager!</h1>
+        </div>
+    )
+}
+
+export default Header
